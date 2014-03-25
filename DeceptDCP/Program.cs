6f4a14ed7125da4faf534e82551685f8042ad36e
dck-jp <dicek.am@clockahead.com>
2014-03-26 01:49:08 +0900

    First Release

---
+++ DeceptDCP/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace DeceptDCP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Count() == 0) {
+                Console.WriteLine("カメラの機種名を引数に指定して、実行してください。");
+                Console.Read();
+                return;
+            }
+            var cameraModel = args[0];
+            var dcpList = GetDCP();
+
+            dcpList.ForEach(dcpFile =>
+            {
+                var xmlFile = DecompileToXML(dcpFile);
+                ModifyXML(xmlFile, cameraModel);
+                CompileToDCP(xmlFile, dcpFile);
+                File.Delete(xmlFile);
+            });
+
+            var path = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\Adobe\CameraRaw\CameraProfiles";
+            Process.Start(path);
+        }
+
+        private static void CompileToDCP(string xmlFile, string dcpFile)
+        {
+            ExecuteDcpTool("-c", xmlFile, dcpFile); 
+        }
+
+        private static void ModifyXML(string xmlFile, string cameraModel)
+        {
+            var text = File.ReadAllText(xmlFile);
+            var re = new Regex("<UniqueCameraModelRestriction>.+</UniqueCameraModelRestriction>");
+            var replaceText = "<UniqueCameraModelRestriction>" + cameraModel + "</UniqueCameraModelRestriction>";
+            File.WriteAllText(xmlFile, re.Replace(text, replaceText));
+        }
+
+        private static string DecompileToXML(string dcpFile)
+        {
+            var xmlFile = Path.GetFileNameWithoutExtension(dcpFile) + ".xml";
+            ExecuteDcpTool("-d", dcpFile, xmlFile);
+            return xmlFile;
+        }
+
+        private static List<string> GetDCP()
+        {
+            return Directory.GetFiles(Environment.CurrentDirectory,"*.dcp").ToList();
+        }
+
+        private static void ExecuteDcpTool(string option,string dcpFile,string xmlFile)
+        {
+            var psi = new ProcessStartInfo();
+            psi.FileName = @"dcpTool.exe";
+            psi.Arguments = option + " \"" + dcpFile + "\" \"" + xmlFile + "\"";
+
+            var p = Process.Start(psi);
+            p.WaitForExit();
+        }
+
+    }
+}
---
+++ readme.txt
@@ -0,0 +1,25 @@
+【ソフト名】DeceptDCP
+【著作権者】D*isuke YAMAKAWA / ClockAhead
+【制作日】2014/3/26
+【種　別】フリーソフト
+【連絡先】support@clockahead.com
+【動作環境】WindowsVista以降 .NET Framework4.5必須
+
+------------------------------------------------------------------------------
+DeceptDCPの簡単な使い方
+------------------------------------------------------------------------------
+
+1. 流用したいカメラプロファイル(*.dcpファイル)をダウンロード
+2. Decept.exeフォルダ内に、カメラプロファイルを移動
+3. Decept.exeの引数に「カメラの機種名」を指定して起動
+　例：　コマンドプロンプトにて > Decept.exe "PENTAX K-5"
+　　　　（エラーが出なければ無言で、*.dcpファイルが書き換えられます）
+4. カメラプロファイルのフォルダが自動で開くので、
+  書き換えられた*.dcpファイルを、その中に移動／コピー
+
+------------------------------------------------------------------------------
+同梱のdcpToolについて
+------------------------------------------------------------------------------
+URL: http://dcptool.sourceforge.net/Introduction.html
+License:
+dcpTool is free software, released under the GPL. However, if you use dcpTool for commercial purposes, you should donate to the project. You can donate by selecting the donate link on dcpTool’s project page at SourceForge.net
